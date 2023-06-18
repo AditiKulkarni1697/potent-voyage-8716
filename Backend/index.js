92fa79b954b4a18997bdff0d1ee24c06209ab487
ankita-barad <ankitabarad2111@gmail.com>
2023-06-18 15:30:03 +0530

    worked on home page

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,9 @@
 // NPM Install
 require("dotenv").config();
 
-
-const path = require("path")
-const bcrypt = require("bcrypt")
-const express = require("express")
+const path = require("path");
+const bcrypt = require("bcrypt");
+const express = require("express");
 
 const cors = require("cors");
 const passport = require("passport");
@@ -24,7 +23,7 @@ const { projectRoute } = require("./routes/project.route");
 const { calenderRouter } = require("./routes/calender.route");
 const { timerRoute } = require("./routes/timer.route");
 const { taskRoute } = require("./routes/task.route");
-const {UserModel} = require("./models/user.model")
+const { UserModel } = require("./models/user.model");
 
 const app = express();
 app.use(express.json());
@@ -34,27 +33,12 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("B26_Time-Trace_Project");
 });
-// app.use(authenticate)
-app.use("/user", userRoute);
 
+app.use("/user", userRoute);
 app.use("/timer", timerRoute);
 app.use("/calender", calenderRouter);
 app.use("/project", projectRoute);
-app.use(authenticate)
 app.use("/task", taskRoute);
-
-// app.get('/auth/google',
-//   passport.authenticate('google', { scope: ['profile','email'] }));
-
-// app.get('/auth/google/callback',
-//   passport.authenticate('google', {successRedirect: '/', failureRedirect: '/login', session : false}),
-//   function(req, res) {
-//     // Successful authentication, redirect home.
-//     console.log(req)
-//     res.redirect('/');
-//   });
-
-// Oauth google passport oauth2
 
 app.use(
   session({
@@ -80,40 +64,43 @@ app.get(
   })
 );
 
-app.get("/auth/google/success",(req,res)=>{
-    res.redirect("http://localhost:5501/Frontend/homepage/index.html")
-})
-
+app.get("/auth/google/success", (req, res) => {
+  res.redirect("http://localhost:5501/Frontend/homepage/index.html");
+});
 
 app.get("/auth/google/failure", (req, res) => {
   res.send("Failed !");
 });
 
-app.get('/protected', async (req, res) => {
-      console.log(req.user)
-    
-      console.log(req.user.emails)
-      
-      const isUserPresent = await UserModel.findOne({email:req.user._json.email})
-      // console.log(isUserPresent)
-      if(!isUserPresent){
-          let password = "12345"
-          const hashPass = await bcrypt.hash(password,4);
-          const user = {
-            name : req.user._json.name,
-            email : req.user._json.email,
-            password : hashPass
-          }
-          
-          const newUser = new UserModel(user)
-          await newUser.save()
-          res.redirect("http://127.0.0.1:5501/Frontend/project_timer_pages/project.html");
-      }else{
-        res.redirect("http://127.0.0.1:5501/Frontend/project_timer_pages/project.html");
-      }
+app.get("/protected", async (req, res) => {
+  console.log(req.user);
+
+  console.log(req.user.emails);
+
+  const isUserPresent = await UserModel.findOne({
+    email: req.user._json.email,
   });
+  // console.log(isUserPresent)
+  if (!isUserPresent) {
+    let password = "12345";
+    const hashPass = await bcrypt.hash(password, 4);
+    const user = {
+      name: req.user._json.name,
+      email: req.user._json.email,
+      password: hashPass,
+    };
 
-
+    const newUser = new UserModel(user);
+    await newUser.save();
+    res.redirect(
+      "http://127.0.0.1:5501/Frontend/project_timer_pages/project.html"
+    );
+  } else {
+    res.redirect(
+      "http://127.0.0.1:5501/Frontend/project_timer_pages/project.html"
+    );
+  }
+});
 
 app.get("/protected", (req, res) => {
   res.redirect("http://127.0.0.1:5501/Frontend/homepage/index.html");
@@ -121,61 +108,60 @@ app.get("/protected", (req, res) => {
 
 //   Github Authentication
 
+app.get("/auth/github", async (req, res) => {
+  const { code } = req.query;
 
-app.get("/auth/github",async(req,res)=>{
-
-    const {code} = req.query
-
-    const accessToken = await fetch("https://github.com/login/oauth/access_token",{
-        method: "POST",
-        headers :{
-            Accept: "application/json",
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            client_id : process.env.GITHUB_CLILENT_ID,
-            client_secret : process.env.GITHUB_CLILENT_SECRET,
-            code 
-        })
-    }).then(response => {
-      return response.json()
-    })
-
-    console.log(accessToken)
-    res.cookie("accessToken",accessToken.access_token)
-
-    const user = await fetch("https://api.github.com/user",{
-      headers : {
-        Authorization : `Bearer ${accessToken.access_token}`
-      }
-    })
-    .then((res) => res.json())
-    .catch((err) => console.log(err))
-
-    console.log(user)
-
-    const useremail = await fetch("https://api.github.com/user/emails",{
-      headers : {
-        Authorization : `Bearer ${accessToken.access_token}`
-      }
-    })
-    .then((res) => res.json())
-    .catch((err) => console.log(err))
-
-    console.log(useremail)
-
-    res.redirect("http://127.0.0.1:5501/Frontend/project_timer_pages/project.html")
-})
-
-app.get('/login',  (req, res) => {
-    res.redirect("http://127.0.0.1:5501/Frontend/login_signup_pages/register.html");
+  const accessToken = await fetch(
+    "https://github.com/login/oauth/access_token",
+    {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        client_id: process.env.GITHUB_CLILENT_ID,
+        client_secret: process.env.GITHUB_CLILENT_SECRET,
+        code,
+      }),
+    }
+  ).then((response) => {
+    return response.json();
   });
 
-  
+  console.log(accessToken);
+  res.cookie("accessToken", accessToken.access_token);
 
+  const user = await fetch("https://api.github.com/user", {
+    headers: {
+      Authorization: `Bearer ${accessToken.access_token}`,
+    },
+  })
+    .then((res) => res.json())
+    .catch((err) => console.log(err));
 
+  console.log(user);
 
+  const useremail = await fetch("https://api.github.com/user/emails", {
+    headers: {
+      Authorization: `Bearer ${accessToken.access_token}`,
+    },
+  })
+    .then((res) => res.json())
+    .catch((err) => console.log(err));
 
+  console.log(useremail);
+
+  res.redirect(
+    "http://127.0.0.1:5501/Frontend/project_timer_pages/project.html"
+  );
+});
+
+app.get("/login", (req, res) => {
+  res.redirect(
+    "http://127.0.0.1:5501/Frontend/login_signup_pages/register.html"
+  );
+});
 
 app.get("/auth/protected", isLoggedIn, (req, res) => {
   res.send("Hello there!");
@@ -192,6 +178,3 @@ app.listen(process.env.PORT || 3000, async () => {
     console.log("Cannot connect to DataBase");
   }
 });
-
-
-
