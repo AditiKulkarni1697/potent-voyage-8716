20ea48401e0810c388d28a4a7a31f50d5abc71f9
ankita-barad <ankitabarad2111@gmail.com>
2023-08-31 10:47:56 +0530

    updated oautht

--- Frontend/login_signup_pages/register.js
+++ Frontend/login_signup_pages/register.js
@@ -2,22 +2,7 @@ let firstName = document.getElementById("firstName");
 let lastName = document.getElementById("lastName");
 let email = document.getElementById("email");
 let password = document.getElementById("password");
-// let confirmPass = document.getElementById("confirm-password");
-// const url = "http://localhost:5501"
-// formEl.addEventListener("click",async(e)=>{
-//     e.preventDefault();
-//     // console.log(obj)
-//     // let response = axios.post(`${url}/register`,obj)
-//     // console.log(response)
-//     let response =await fetch(`${url}/register`,{
-//         method : "POST",
-//         headers : {
-//             "Content-Type" : "appllication/json"
-//         },
-//         body : JSON.stringify(obj)
-//     })
-//     console.log(response.json())
-// })
+
 const onsignup = async () => {
   let obj = {
     firstName: firstName.value,
